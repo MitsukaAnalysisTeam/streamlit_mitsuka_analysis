e680e61a57e4a5e46e06616c1e30cd60f4c43ab1
“09B22071” <09B22071@localhost>
2025-02-05 17:30:23 +0900

    hourly_analysis

--- src/components/utils.py
+++ src/components/utils.py
@@ -28,6 +28,7 @@ class DailyReportAnalysisUtils:
                 .str.replace('人', '', regex=True)
                 .str.replace('¥', '', regex=True)
                 .str.replace(',', '', regex=True)
+                .str.replace('#DIV/0!','0', regex=True)
             )
         df['客単価(夜)'] = df['客単価(夜)'].replace('#DIV/0!', '0')
         df['客数(夜)'] = df['客数(夜)'].astype(int)
@@ -155,7 +156,72 @@ class DailyReportAnalysisUtils:
                     print(f"エラーが発生しました: 年={year}, 月={month}, {e}")
         # print(df_weekday_dic)
         return df_weekday_dic
+    
+class HourlyReportAnalysisUtils:
+    '''
+    時間別データ用のメソッド追加クラス
+    '''
+    def convert_hourly_report_data(self, df: pd.DataFrame) -> pd.DataFrame:
+        # 必要なカラムだけを抽出し、全ての値が0の行を削除
+        df = df.loc[:, ['11時', '12時', '13時', '14時', '15時', '16時', '17時', '18時', '19時', '20時', '21時', '22時', '23時']].dropna()
+        df = df.loc[(df != 0).any(axis=1)]
+        # インデックスを datetime 型に変換（保持）
+        df.index = pd.to_datetime(df.index)
 
+        # インデックスから曜日を取得し、"曜日" カラムとして追加
+        df["曜日"] = df.index.dayofweek.map({0: '月', 1: '火', 2: '水', 3: '木', 4: '金', 5: '土', 6: '日'})
+        
+        # 表示の際にインデックスをフォーマット
+        df.index = df.index.map(lambda x: x.strftime("%Y-%m-%d"))
+        return df
+
+    
+    def get_cus_file_path_by_date(
+            self,
+            date: str
+            )-> str: 
+        # データフォルダのパス
+        data_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../data/processed/hourly_report/customer_num"))
+        
+        # ディレクトリ内のCSVファイルをリストアップ
+        csv_files = [f for f in os.listdir(data_dir) if f.endswith('.csv')]
+
+        matched_file = [s for s in csv_files if re.match(f'.*{date}.*', s)]
+
+        # ファイルが見つかった場合はフルパスを返す
+        if matched_file:
+            return os.path.join(data_dir, matched_file[0])
+        
+        # 見つからない場合は空文字列を返す
+        return ""
+    
+    def get_sales_file_path_by_date(
+            self,
+            date: str
+            )-> str: 
+        # データフォルダのパス
+        data_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../data/processed/hourly_report/sales_sum"))
+        
+        # ディレクトリ内のCSVファイルをリストアップ
+        csv_files = [f for f in os.listdir(data_dir) if f.endswith('.csv')]
+
+        matched_file = [s for s in csv_files if re.match(f'.*{date}.*', s)]
+
+        # ファイルが見つかった場合はフルパスを返す
+        if matched_file:
+            return os.path.join(data_dir, matched_file[0])
+        
+        # 見つからない場合は空文字列を返す
+        return ""
+    
+    def get_week_groupby_mean(
+            self,
+            df: pd.DataFrame
+            )->pd.DataFrame:
+        week_group_mean = df.groupby('曜日').mean().round(1)
+        week_group_mean = week_group_mean.reindex(index=['水', '木', '金','土','日'])
+        return week_group_mean
+    
 
 def get_month_list():
     now = datetime.now() - relativedelta(months=1)
--- src/pages/home.py
+++ src/pages/home.py
@@ -1,5 +1,4 @@
 import streamlit as st
 
 def show():
-    st.title("BI Dashboard - Home")
-    st.write("これはホームページです。")
+    st.write("トップページ")
