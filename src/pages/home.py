28294177f04d2fa81403000b4fd04d7b703ebf53
“09B22071” <09B22071@localhost>
2025-02-09 01:25:02 +0900

    change

--- src/pages/home.py
+++ src/pages/home.py
@@ -2,39 +2,28 @@ import streamlit as st
 import pandas as pd
 from datetime import datetime
 import os
+import src.components.utils as utils
 
 # フィードバックを保存するファイル名
 FEEDBACK_FILE = data_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../data/raw/feedback/feedback.csv"))
 
 def save_feedback(question):
     """意見をCSVファイルに保存"""
-    # 現在の日付を取得
-    current_date = datetime.now().strftime("%Y-%m-%d")
-
-    # ファイルが存在するか確認
-    if os.path.exists(FEEDBACK_FILE):
-        df = pd.read_csv(FEEDBACK_FILE)
-    else:
-        df = pd.DataFrame(columns=["日付", "意見"])
-
-    # 新しいデータを追加
-    new_data = pd.DataFrame({"日付": [current_date], "意見": [question]})
-    df = pd.concat([df, new_data], ignore_index=True)
-
-    # CSVファイルとして保存
-    df.to_csv(FEEDBACK_FILE, index=False)
+    spreadsheet = utils.SpreadSheets()
+    current_date = datetime.now()
+    spreadsheet.write_feedback(date=current_date,text=question)
 
 def show():
     st.write("ホームページ")
 
     # 質問受付フォームの追加
     with st.form(key="question_form"):
-        question = st.text_area("意見がある場合はここへ↓↓\nみなさんのフィードバックでより良い分析ページを作っていきましょう！")
+        question = st.text_area("意見がある場合はここへ↓↓\n\nみなさんのフィードバックでより良い分析ページを作っていきましょう！")
         submit_button = st.form_submit_button(label="送信")
 
     if submit_button:
         if question:
-            save_feedback(question)  # CSVに保存
+            save_feedback(question)  
             st.success(f"意見を受け付けました！\n\n**質問:** {question}")
         else:
             st.error("すべての項目を入力してください。")
